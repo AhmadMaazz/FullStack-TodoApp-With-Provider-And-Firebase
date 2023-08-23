d7543d6933039cc8addee0d53e22b752e618bf07
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-08-23 23:20:55 +0500

    added drawer to homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fullstack_todo_app/firebase/firebase.utils.dart';
+import 'package:fullstack_todo_app/widgets/rps_custompainter.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final User? user = Auth().currentUser;
 
   Future<void> signOut() async {
@@ -14,48 +20,90 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double textSize = MediaQuery.textScaleFactorOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Profile'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: size.height * 0.17,
+                child: CustomPaint(
+                  size: Size(
+                      size.width,
+                      (size.width * 0.2833333333333334)
+                          .toDouble()), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+                  painter: RPSCustomPainter(),
+                ),
+              ),
+              Positioned(
+                left: size.width * 0.25,
+                right: size.width * 0.2,
+                top: size.height * 0.06,
+                child: ListTile(
+                  leading: const CircleAvatar(
+                    backgroundColor: Colors.pink,
+                  ),
+                  title: Text('Hello! ${user?.displayName ?? 'User email'}'),
+                ),
+              ),
+              Positioned(
+                top: 50, // Adjust the top position as needed
+                left: 10, // Adjust the left position as needed
+                child: Builder(
+                  // Wrap the IconButton with a Builder widget
+                  builder: (BuildContext context) {
+                    return IconButton(
+                      icon: const Icon(Icons.menu), // Icon for the drawer
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer(); // Open the drawer
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          Text(
+            user?.email ?? 'User email',
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+            ),
+            onPressed: () async {
+              await signOut();
+              if (context.mounted) Navigator.pop(context);
+            },
+            child: const Text('sign out'),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              user?.email ?? 'User email',
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+            const DrawerHeader(
+              child: Icon(Icons.favorite),
             ),
-            const SizedBox(height: 10),
-            const Text(
-              'User ID: ',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+            ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('D A S H B O A R D'),
+                onTap: () {}),
+            const ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('L O G O U T'),
             ),
-            const SizedBox(height: 10),
-            const Text(
-              'User Name: ',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange,
-              ),
-              onPressed: () async {
-                await signOut();
-                if (context.mounted) Navigator.pop(context);
-              },
-              child: const Text('sign out'),
+            const ListTile(
+              leading: Icon(Icons.info),
+              title: Text('T E R M S & P O L I C Y'),
             ),
           ],
         ),
