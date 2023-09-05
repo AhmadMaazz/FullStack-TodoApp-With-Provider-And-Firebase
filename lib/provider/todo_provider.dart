2655de17c5055cb9e417c3c3491833efb9bdff47
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-09-06 02:09:48 +0500

    completed CRUD for the app

--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -2,7 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fullstack_todo_app/models/todo.dart';
 
+import '../firebase/firebase.utils.dart';
+
 class TodoProvider extends ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List<Todo> _todos = [];
 
   List<Todo> get todos => _todos;
@@ -12,9 +15,40 @@ class TodoProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void removeTodo(int index) {
-    _todos.removeAt(index);
-    notifyListeners();
+  // void removeTodo(int index) {
+  //   _todos.removeAt(index);
+  //   notifyListeners();
+  // }
+
+  void removeTodo(int index) async {
+    // Check if the index is within the valid range
+    if (index >= 0 && index < _todos.length) {
+      final Todo todo = _todos[index];
+
+      // Remove the task from Firestore
+      final user = Auth().currentUser;
+      if (user != null) {
+        final userTasksRef =
+            _firestore.collection('users').doc(user.uid).collection('tasks');
+
+        // Retrieve the list of tasks for the user
+        final tasksSnapshot = await userTasksRef.get();
+
+        // Find the Firestore-generated document ID of the task to delete
+        final taskToDelete = tasksSnapshot.docs.firstWhere((doc) =>
+            doc['taskName'] == todo.taskName &&
+            doc['description'] == todo.description &&
+            doc['creationTime'] == todo.creationTime);
+
+        if (taskToDelete != null) {
+          await userTasksRef.doc(taskToDelete.id).delete();
+        }
+      }
+
+      // Remove the task from the local list and update the UI
+      _todos.removeAt(index);
+      notifyListeners();
+    }
   }
 
   void toggleTodoCompletion(int index) {
@@ -22,16 +56,61 @@ class TodoProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void updateTask(Todo todo, String newTaskName, String newTaskDescription) {
+  // void updateTask(Todo todo, String newTaskName, String newTaskDescription) {
+  //   final index = _todos.indexOf(todo);
+  //   if (index != -1) {
+  //     _todos[index] = Todo(
+  //       taskName: newTaskName,
+  //       description: newTaskDescription,
+  //       creationTime: todo.creationTime,
+  //       isCompleted: todo.isCompleted,
+  //     );
+  //     notifyListeners();
+  //   }
+  // }
+
+  void updateTask(
+      Todo todo, String newTaskName, String newTaskDescription) async {
+    // Find the index of the task in the local list
     final index = _todos.indexOf(todo);
+
     if (index != -1) {
-      _todos[index] = Todo(
-        taskName: newTaskName,
-        description: newTaskDescription,
-        creationTime: todo.creationTime,
-        isCompleted: todo.isCompleted,
-      );
-      notifyListeners();
+      // Find the Firestore-generated document ID of the task to update
+      final user = Auth().currentUser;
+      if (user != null) {
+        final userTasksRef =
+            _firestore.collection('users').doc(user.uid).collection('tasks');
+
+        // Retrieve the list of tasks for the user
+        final tasksSnapshot = await userTasksRef.get();
+
+        // Find the Firestore-generated document ID of the task to update
+        final taskToUpdate = tasksSnapshot.docs.firstWhere(
+          (doc) =>
+              doc['taskName'] == todo.taskName &&
+              doc['description'] == todo.description &&
+              doc['creationTime'] == todo.creationTime,
+        );
+
+        if (taskToUpdate != null) {
+          final taskId = taskToUpdate.id;
+
+          // Update the task in Firestore
+          await userTasksRef.doc(taskId).update({
+            'taskName': newTaskName,
+            'description': newTaskDescription,
+          });
+
+          // Update the task in the local list and update the UI
+          _todos[index] = Todo(
+            taskName: newTaskName,
+            description: newTaskDescription,
+            creationTime: todo.creationTime,
+            isCompleted: todo.isCompleted,
+          );
+          notifyListeners();
+        }
+      }
     }
   }
 
@@ -60,5 +139,4 @@ class TodoProvider extends ChangeNotifier {
       debugPrint('Error fetching user tasks: $error');
     }
   }
-
 }
